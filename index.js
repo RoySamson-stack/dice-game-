20270e6b4c4141d23dc5e6b0b8c33e1cb08ff47f
RoySamson-stack <rsam9009@gmail.com>
2021-12-09 13:24:12 +0300

    finishing the script that on refresh a different dice is shown

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-var randomNumber1 = Math.floor(Math.random()* 6)
+var randomNumber1 = Math.floor(Math.random()* 6) + 1;
+var randomNumber2 = Math.floor(Math.random()* 6) + 1;
+
+
+//get random images for dice
+var randomImage1 = "images/dice" + randomNumber1 + ".png"
+//set the image using setAttribute
+document.querySelectorAll("img")[0].setAttribute("src", randomImage1);
+
+
+var randomImage2 = "images/dice" + randomNumber2 + ".png"
+document.querySelectorAll("img")[1].setAttribute("src", randomImage2);
